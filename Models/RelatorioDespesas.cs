8d4dccda426cc53d1c3b2c5bbb4a297778ca5e5d
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-04 17:37:44 -0300

    feat: Adicionando record RelatorioDespesas

---
+++ Models/RelatorioDespesas.cs
@@ -0,0 +1,8 @@
+namespace DesafioTecnicoC.Models;
+public record RelatorioDespesas{
+  public int Id {get;init;}
+  public string Nome {get;init;}=string.Empty;
+  public float TotalReceitas {get;}
+  public float TotalDespesas {get;}
+  public float Saldo {get;}
+}
