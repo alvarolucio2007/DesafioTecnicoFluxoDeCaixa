84c3d2df1dca7be7dad23f67e4b1c4c4b56c5da2
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-04 12:30:28 -0300

    feat: Adicionando PessoaRepository

---
+++ Repositories/PessoaRepository.cs
@@ -0,0 +1,69 @@
+using System.Data;
+using Microsoft.Data.Sqlite;
+using Dapper;
+
+using DesafioTecnicoC.Models;
+
+namespace DesafioTecnicoC.Repositories;
+
+public class PessoaRepository
+{
+    private readonly string _connectionString;
+    public PessoaRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    ///<summary>
+    ///Insere uma nova pessoa no banco de dados.
+    ///</summary>
+    ///<param name="pessoa"> Objeto contendo os dados da pessoa (nome e idade).</param>
+    public async Task InserirAsync(Pessoa pessoa)
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        string sql = "INSERT INTO pessoas (nome, idade) VALUES (@Nome, @Idade);";
+        await connection.ExecuteAsync(sql, pessoa);
+    }
+    ///<summary>
+    ///Lista todas as pessoas cadastradas no banco de dados.
+    ///</summary>
+    public async Task<IEnumerable<Pessoa>> ListarTodosAsync()
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        string sql = "SELECT id,nome,idade FROM pessoas;";
+        return await connection.QueryAsync<Pessoa>(sql);
+    }
+    ///<summary>
+    ///Busca uma pessoa em específico através do ID
+    ///</summary>
+    ///<param name="id">ID da pessoa buscada.</param>
+    public async Task<Pessoa?> BuscarPorIdAsync(int id)
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        string sql = "SELECT id,nome,idade FROM pessoas WHERE id = @Id;";
+        return await connection.QueryFirstOrDefaultAsync<Pessoa>(sql, new { Id = id });
+    }
+    ///<summary>
+    ///Atualiza uma pessoa através do ID dela.
+    ///</summary>
+    ///<param name="id">ID da pessoa editada.</param>
+    ///<param name="pessoa">Novos dados da pessoa editada.</param>
+    public async Task AtualizarPessoa(int id, Pessoa pessoa)
+    {
+
+        using var connection = new SqliteConnection(_connectionString);
+        string sql = "UPDATE pessoas SET nome = @Nome, Idade = @Idade WHERE id=@Id";
+        await connection.ExecuteAsync(sql, new { Id = id, Nome = pessoa.Nome, Idade = pessoa.Idade });
+    }
+    ///<summary>
+    ///Remove uma pessoa através do ID dela.
+    ///</summary>
+    ///<param name="id">ID da pessoa a ser removida. </param>
+    public async Task DeletarPessoa(int id)
+    {
+
+        using var connection = new SqliteConnection(_connectionString);
+        string sql = "DELETE FROM pessoas WHERE id=@Id";
+        await connection.ExecuteAsync(sql, new { Id = id });
+    }
+
+}
