b2817f9d3be361452a5b0bb1b0010e1258a3fc5a
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-04 17:51:01 -0300

    fix: Renomeando RelatorioDespesas para RelatorioIndivual,

--- Models/RelatorioIndividual.cs
+++ Models/RelatorioIndividual.cs
@@ -1,5 +1,5 @@
 namespace DesafioTecnicoC.Models;
-public record RelatorioDespesas{
+public record RelatorioIndividual{
   public int Id {get;init;}
   public string Nome {get;init;}=string.Empty;
   public float TotalReceitas {get;}
