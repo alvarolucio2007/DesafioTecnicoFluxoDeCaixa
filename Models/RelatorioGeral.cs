961c55a1cf1dbd2a59bebcf3776efa420f2ebbe6
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-04 17:51:08 -0300

    feat: Adicionando record RelatorioGeral

---
+++ Models/RelatorioGeral.cs
@@ -0,0 +1,7 @@
+namespace DesafioTecnicoC.Models;
+public record RelatorioGeral
+{
+  public float Total_Geral_Receitas {get;}
+  public float Total_Geral_Despesas {get;}
+  public float Saldo_Liquido_Geral {get;}
+}
